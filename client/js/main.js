6b6b4ca15218d1d433c5fe4547ecf39bcce4d15e
Iulian <iulian.petru.peta@gmail.com>
2019-08-11 20:23:29 +0300

    show clients dropdown

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,26 @@
 $(function() {
-    alert('jquery on');
+    $.get(
+    	'http://localhost/hotel/server/clients.php',
+    	function(data) {
+    		parseClientsData(data);
+    	}
+    );
 });
+
+function parseClientsData(clientsList) {
+	clientsList.map(parseClient);
+}
+
+function parseClient(clientData) {
+	clientElement = createOptionFromClientData(clientData);
+	$('#clients_menu').append(clientElement);
+}
+
+function createOptionFromClientData(clientData) {
+
+	option = $('<option></option>');
+	option.attr('value', clientData.id);
+	option.text(clientData.name);
+
+	return option;
+}
