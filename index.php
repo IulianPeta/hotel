7cd5fbba88ddaffef33cebea13f4faeab3c13cf4
Iulian <iulian.petru.peta@gmail.com>
2019-08-11 18:53:12 +0300

    Add booking interface

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+
+
+<body>
+		
+	<form>
+		<table>
+			<tr>
+				<td>
+					<label>Client</label>
+				</td>
+				<td>
+					<select>
+						<option value="">	</option>
+					</select>
+				</td>
+
+			</tr>
+
+			<tr>
+				<td>
+					<label>Room</label>
+				</td>
+				<td>
+					<select>
+						<option value="">	</option>
+					</select>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+					<label>Start</label>
+				</td>
+				<td>
+					<input type="text" name="Start">
+				</td>
+
+			</tr>
+
+			<tr>
+				<td>
+					<label>End</label>
+				</td>
+				<td>
+					<input type="text" name="End">
+				</td>
+
+			</tr>
+
+		</table>
+
+		<button type="submit">Submit</button>
+	</form>
+</body>
+</html>
