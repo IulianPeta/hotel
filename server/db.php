236907654055ead599fe9b91be466328c4c982e8
Romi Halasz <novocainex@gmail.com>
2019-08-11 19:39:43 +0300

    Fetch all rows.

--- server/db.php
+++ server/db.php
@@ -4,9 +4,10 @@
 
 	$result = $mysqli->query('SELECT * FROM clients');
 
-	while($row = $result->fetch_assoc()) 
-	{
-		print_r($row);
-	}
+	// while($row = $result->fetch_assoc()) 
+	// {
+	// 	print_r($row);
+	// }
 
+	print_r($result->fetch_all('MYSQLI_ASSOC'));
 ?>
